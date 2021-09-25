106ae294ff56a7078a3ee384d2887a5ce2d70b2e
Anderson <and_mendescosta@hotmail.com>
2021-09-24 22:35:17 -0300

    Correção

--- heart.js
+++ heart.js
@@ -1,4 +1,7 @@
 var likes = document.querySelectorAll('.js-like');
+var filtro = document.querySelectorAll('.procura_titulo');
+var noticias = document.querySelectorAll('.noticia');
+var input = document.querySelector('.procura_input');
 
 for (let like of likes) {
     like.addEventListener('click', () => liked(like));
@@ -13,3 +16,12 @@ function liked(item) {
         item.classList.add('far')
     }
 }
+
+input.onkeyup = function() {
+    var valorInput = input.value;
+    for (var i = 0; i < noticias.length; i++) {
+        var conteudoTitulo = filtro[i].innerText;
+        var corresponde = conteudoTitulo.toLowerCase().indexOf(valorInput) >= 0;
+        noticias[i].style.display = corresponde ? '' : 'none';
+    }
+};
